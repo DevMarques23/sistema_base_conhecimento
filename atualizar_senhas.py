fe18c53151603402194116571529950e4f94465e
DevMarques23 <ti.brunom@gmail.com>
2025-02-03 15:55:25 -0300

    ajuste de lógica de criptografia de senha

--- atualizar_senhas.py
+++ atualizar_senhas.py
@@ -3,7 +3,7 @@ from app import app, db, Usuario
 with app.app_context():
     usuarios = Usuario.query.all()
     for usuario in usuarios:
-        if not usuario.password.startswith('pbkdf2:'):  # Verifica se a senha já está criptografada
+        if not usuario.password.startswith('scrypt:'):  # Verifica se a senha já está criptografada
             usuario.set_password(usuario.password)  # Criptografa a senha
             db.session.commit()
             print(f"Senha do usuário {usuario.username} atualizada com sucesso.")
